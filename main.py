09d59bd0ea696efc912efb7e7f28917a5dc0e416
janri1969 <checkerok@yahoo.com>
2022-09-09 19:56:32 +0700

    start

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """
 Aplikasi BMKG
 """
-from scrap_bmkg import data_extraction, data_show
+from scrap_berita import data_extraction, data_show
 
 if __name__ == '__main__':
-    print("\nScrapping Data from BMKG")
+#    print("\nScrapping Data from BMKG")
+    print("\nScrapping Data from Detik.com")
     result = data_extraction()
     data_show(result)
 
---
+++ scrap_berita/__init__.py
@@ -0,0 +1,72 @@
+"""
+Aplikasi Scrap data Berita terpopuler Detik.com
+"""
+import requests
+from bs4 import BeautifulSoup
+
+def data_extraction():
+#   print("\nData Mentah dari BMKG")
+    content = requests.get("https://detik.com")
+    soup = BeautifulSoup(content.text, "html.parser")
+    print(soup)
+    result = soup.find('div', {'class' : 'media__text text-list'})
+    result = result.findChildren('div')
+    print(result)
+
+    i = 0
+
+    waktu = None
+    tanggal = None
+    magnitudo = None
+    kedalaman = None
+    ls = None
+    bt = None
+    lokasi = None
+    dirasakan = None
+
+    for res in result:
+
+        if i == 0:
+            time_date = res.text.split(', ')
+            waktu = time_date[1]
+            tanggal = time_date[0]
+#            print(time_date)
+        elif i == 1:
+            magnitudo = res.text
+#            print(magnitudo)
+        elif i == 2:
+            kedalaman = res.text
+#            print(kedalaman)
+        elif i == 3:
+            koordinat = res.text.split('- ')
+            ls = koordinat[0]
+            bt = koordinat[1]
+#            print(koordinat)
+        elif i == 4:
+            lokasi = res.text
+#            print(lokasi)
+        elif i == 5:
+            dirasakan = res.text
+#            print(dirasakan)
+        i = i + 1
+
+    hasil = dict()
+    hasil['time_date'] = {'waktu' : waktu, 'tanggal' : tanggal}
+    hasil['magnitudo'] = magnitudo
+    hasil['kedalaman'] = kedalaman
+    hasil['koordinat'] = {'ls' : ls, 'bt' : bt }
+    hasil['lokasi'] = lokasi
+    hasil['dirasakan'] = dirasakan
+    return hasil
+
+def data_show(result):
+
+    # print("\nGempa Terakhir berdasarkan BMKG")
+    # print(f"Waktu : {result['time_date']['waktu']}")
+    # print(f"Tanggal : {result['time_date']['tanggal']}")
+    # print(f"Magnitudo : {result['magnitudo']}")
+    # print(f"Kedalaman : {result['kedalaman']}")
+    # print(f"Koordinat : LS = {result['koordinat']['ls']}, BT = {result['koordinat']['bt']}")
+    # print(f"Lokasi : {result['lokasi']}")
+    # print(f"{result['dirasakan']}")
+    print("data show")
